d46c5643e090562475352143931a6e7952700d45
Paul E. Long <paulelong@outlook.com>
2023-05-20 12:04:23 -0400

    get logs using adb

--- elogcat.ps1
+++ elogcat.ps1
@@ -1,12 +1,25 @@
-
-
 $templogfile = ".\templog.log"
 
 $notdone = $true
 
+$filter=$args[0]
+
+if($args[1] -ne $null)
+{
+    $tid = "-t " + $args[1]
+}
+
 while($notdone)
 {
-    adb logcat -t ((Get-Date).AddMinutes(-5).ToString("MM-dd HH:ss:mm.ffff")) > $templogfile
+    if($args[1] -ne $null)
+    {
+        adb -t $args[1] logcat -t ((Get-Date).AddMinutes(-5).ToString("MM-dd HH:ss:mm.ffff")) > $templogfile
+    }
+    else
+    {
+        adb logcat -t ((Get-Date).AddMinutes(-5).ToString("MM-dd HH:ss:mm.ffff")) > $templogfile
+    }
+
     if((dir $templogfile).Length -gt 0)
     {
         $notdone = $false
@@ -17,7 +30,7 @@ while($notdone)
 
 if($notdone -eq $false)
 {
-    &"E:\Users\Paul\Downloads\TextAnalysisTool.NET\Latest Without Plug-in Support\TextAnalysisTool.NET.exe" $templogfile /Filters:$args
+    &"E:\Users\Paul\Downloads\TextAnalysisTool.NET\Latest Without Plug-in Support\TextAnalysisTool.NET.exe" $templogfile /Filters:$filter
 }
 else 
 {
